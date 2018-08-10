40028ec7ebbdd0edfce575b5feccc5799b4de9ef
Matt Moriarity <matt@mattmoriarity.com>
2018-08-09 19:11:34 -0500

    Redirect to Twitter auth if not logged in
    Eventually, this should probably be a marcom landing page or something,
    but this is good for now.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  before_action :ensure_logged_in
+
   def index; end
+
   def feeds; end
+
+  private
+
+  def ensure_logged_in
+    redirect_to '/auth/twitter' unless session[:user]
+  end
 end
