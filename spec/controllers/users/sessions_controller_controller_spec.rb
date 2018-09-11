4dddbaf2de7473bc0935fb2db8e0568eaa601348
Matt Moriarity <matt@mattmoriarity.com>
2018-09-10 19:33:25 -0500

    Add specs for feed model

--- spec/controllers/users/sessions_controller_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Users::SessionsControllerController, type: :controller do
-
-end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :feeds, :users, :feed_subscriptions
+
+  describe '#to_message' do
+    it 'represents a simple feed' do
+      feed = feeds(:example)
+      expect(feed.to_message).to eq FeedMessage.new(
+        id: feed.id,
+        url: 'https://example.org/feed.json',
+        title: 'Example.org',
+        home_page_url: 'https://example.org/',
+        created_at: feed.created_at.to_s(:iso8601),
+        updated_at: feed.updated_at.to_s(:iso8601)
+      )
+    end
+
+    it 'represents a refreshed feed' do
+      feed = feeds(:refreshed_example)
+      expect(feed.to_message).to eq FeedMessage.new(
+        id: feed.id,
+        url: 'https://example.com/feed.json',
+        title: 'Example.com',
+        home_page_url: 'https://example.com/',
+        refreshed_at: '2018-01-01T00:00:00Z',
+        created_at: feed.created_at.to_s(:iso8601),
+        updated_at: feed.updated_at.to_s(:iso8601)
+      )
+    end
+  end
+
+  describe '.register' do
+    context 'when the feed has never been registered' do
+      let(:feed) do
+        Feed.register(users(:alice), url: 'https://foo.example.org/feed.json')
+      end
+
+      it 'creates a new feed' do
+        expect { feed }.to change { Feed.count }.by 1
+      end
+
+      it 'sets the URL for the feed' do
+        expect(feed.url).to eq 'https://foo.example.org/feed.json'
+      end
+
+      it 'creates a subscription for the user registering' do
+        feed
+        expect(users(:alice).feed_ids).to include(feed.id)
+      end
+    end
+
+    context 'when the feed is registered to another user' do
+      let(:feed) do
+        Feed.register(users(:bob), url: 'https://example.org/feed.json')
+      end
+
+      it 'does not create a new feed' do
+        expect(feed).to eq feeds(:example)
+      end
+
+      it 'creates a subscription for the user registering' do
+        feed
+        expect(users(:bob).feed_ids).to include(feed.id)
+      end
+    end
+  end
 end
