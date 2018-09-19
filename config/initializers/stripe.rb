b6b076782eab4611125c87d55402959ffd0b0f36
Matt Moriarity <matt@mattmoriarity.com>
2018-09-18 20:15:17 -0500

    Allow running production app in a different Stripe config

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
-Rails.configuration.stripe = Rails.application.credentials.stripe[Rails.env.to_sym]
+# This lets us put staging into the dev payment environment
+stripe_env = (ENV['STRIPE_ENV'] || Rails.env).to_sym
+Rails.configuration.stripe = Rails.application.credentials.stripe[stripe_env]
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
