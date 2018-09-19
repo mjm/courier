2077d8595996b71f589b5a839bdd0e442b0c83ab
Matt Moriarity <matt@mattmoriarity.com>
2018-09-18 20:29:42 -0500

    Move plans into Stripe initializer

--- config/initializers/plans.rb
+++
@@ -1,9 +0,0 @@
-require 'plan'
-
-Plan::MONTHLY = Plan.new(
-  key: :monthly,
-  name: 'Monthly Plan',
-  plan_id: STRIPE_ENV == :production ? 'plan_DcPlEgFAqBYpnH' : 'plan_DcLKc40R2MpDkG',
-  amount: 500,
-  interval: 'month'
-)
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,14 @@
+require 'plan'
+
 # This lets us put staging into the dev payment environment
 STRIPE_ENV = (ENV['STRIPE_ENV'] || Rails.env).to_sym
 Rails.configuration.stripe = Rails.application.credentials.stripe[stripe_env]
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+Plan::MONTHLY = Plan.new(
+  key: :monthly,
+  name: 'Monthly Plan',
+  plan_id: STRIPE_ENV == :production ? 'plan_DcPlEgFAqBYpnH' : 'plan_DcLKc40R2MpDkG',
+  amount: 500,
+  interval: 'month'
+)
