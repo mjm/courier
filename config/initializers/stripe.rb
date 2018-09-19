779d15d4f7446bd01fa8bc6f873b5a5a0959a220
Matt Moriarity <matt@mattmoriarity.com>
2018-09-18 20:24:04 -0500

    Use appropriate plan name for Stripe environment

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 # This lets us put staging into the dev payment environment
-stripe_env = (ENV['STRIPE_ENV'] || Rails.env).to_sym
+STRIPE_ENV = (ENV['STRIPE_ENV'] || Rails.env).to_sym
 Rails.configuration.stripe = Rails.application.credentials.stripe[stripe_env]
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
