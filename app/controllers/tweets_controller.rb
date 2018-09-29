8f5e78bbb8e48950b0f606ff5540f26569abd2c8
Matt Moriarity <matt@mattmoriarity.com>
2018-09-28 22:02:50 -0500

    Finish specs for tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,39 +6,42 @@ class TweetsController < ServiceController
   end
 
   def cancel_tweet(req, env)
-    require_user env do |user|
-      tweet = user.tweets.find(req.id)
+    require_tweet env, req.id do |tweet|
       tweet.canceled!
       CancelTweetResponse.new(tweet: tweet.to_message)
-    rescue ActiveRecord::RecordNotFound
-      Twirp::Error.not_found "Could not find tweet #{req.id}"
     end
   end
 
   def uncancel_tweet(req, env)
-    require_user env do |user|
-      tweet = user.tweets.find(req.id)
+    require_tweet env, req.id do |tweet|
       tweet.draft!
       UncancelTweetResponse.new(tweet: tweet.to_message)
     end
   end
 
   def update_tweet(req, env)
-    require_user env do |user|
-      tweet = user.tweets.find(req.id)
+    require_tweet env, req.id do |tweet|
       tweet.update body: req.body
-
       tweet.post_to_twitter if req.should_post
-
       UpdateTweetResponse.new(tweet: tweet.to_message)
     end
   end
 
   def post_tweet(req, env)
-    require_user env do |user|
-      tweet = user.tweets.find(req.id)
+    require_tweet env, req.id do |tweet|
       tweet.post_to_twitter
       PostTweetResponse.new(tweet: tweet.to_message)
+    end
+  end
+
+  private
+
+  def require_tweet(env, id)
+    require_user env do |user|
+      tweet = user.tweets.find(id)
+      yield tweet
+    rescue ActiveRecord::RecordNotFound
+      Twirp::Error.not_found "Could not find tweet #{id}"
     end
   end
 end
--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -17,6 +17,26 @@ RSpec.describe TweetsController, type: :rpc do
     include_examples 'an unauthenticated request'
   end
 
+  shared_examples 'a request for a missing tweet' do
+    context 'when the tweet does not exist' do
+      let(:request) { { id: 123 } }
+
+      it 'responds with a not found error' do
+        expect(response).to be_a_twirp_error :not_found
+      end
+    end
+  end
+
+  shared_examples 'a request from a different user' do
+    context 'when the tweet belongs to someone else' do
+      let(:current_user) { users(:bob) }
+
+      it 'responds with a not found error' do
+        expect(response).to be_a_twirp_error :not_found
+      end
+    end
+  end
+
   describe '#cancel_tweet' do
     rpc_method :CancelTweet
     let(:tweet) { tweets(:alice_example_status) }
@@ -32,33 +52,87 @@ RSpec.describe TweetsController, type: :rpc do
     end
 
     include_examples 'an unauthenticated request'
-
-    context 'when the tweet does not exist' do
-      let(:request) { { id: 123 } }
-
-      it 'responds with a not found error' do
-        expect(response).to be_a_twirp_error :not_found
-      end
-    end
-
-    context 'when the tweet belongs to someone else' do
-      let(:current_user) { users(:bob) }
-
-      it 'responds with a not found error' do
-        expect(response).to be_a_twirp_error :not_found
-      end
-    end
+    include_examples 'a request for a missing tweet'
+    include_examples 'a request from a different user'
   end
 
   describe '#uncancel_tweet' do
     rpc_method :UncancelTweet
+    let(:tweet) { tweets(:alice_example_status) }
+    let(:request) { { id: tweet.id } }
+
+    before do
+      tweet.canceled!
+    end
+
+    it 'moves the tweet to the draft status' do
+      response
+      expect(tweet.reload).to be_draft
+    end
+
+    it 'responds with the updated tweet' do
+      expect(response.tweet.status).to eq :DRAFT
+    end
+
+    include_examples 'an unauthenticated request'
+    include_examples 'a request for a missing tweet'
+    include_examples 'a request from a different user'
   end
 
   describe '#update_tweet' do
     rpc_method :UpdateTweet
+    let(:tweet) { tweets(:alice_example_status) }
+    let(:request) { { id: tweet.id, body: 'Foo bar baz' } }
+
+    it 'updates the body of the tweet' do
+      response
+      expect(tweet.reload.body).to eq 'Foo bar baz'
+    end
+
+    it 'responds with the updated tweet' do
+      expect(response.tweet.body).to eq 'Foo bar baz'
+      expect(response.tweet.will_post_at).to be_blank
+    end
+
+    it 'does not post the tweet by default' do
+      response
+      expect(PostTweetsWorker).not_to have_enqueued_sidekiq_job(anything)
+    end
+
+    context 'when should_post is true' do
+      let(:request) { { id: tweet.id, body: 'Foo bar baz', should_post: true } }
+
+      it 'posts the tweet after updating it' do
+        response
+        expect(PostTweetsWorker).to have_enqueued_sidekiq_job([tweet.id])
+      end
+
+      it 'responds with the updated tweet' do
+        expect(response.tweet.will_post_at).to be_present
+      end
+    end
+
+    include_examples 'an unauthenticated request'
+    include_examples 'a request for a missing tweet'
+    include_examples 'a request from a different user'
   end
 
   describe '#post_tweet' do
     rpc_method :PostTweet
+    let(:tweet) { tweets(:alice_example_status) }
+    let(:request) { { id: tweet.id } }
+
+    it 'enqueues a job to post the tweet' do
+      response
+      expect(PostTweetsWorker).to have_enqueued_sidekiq_job([tweet.id])
+    end
+
+    it 'responds with the updated tweet' do
+      expect(response.tweet.will_post_at).to be_present
+    end
+
+    include_examples 'an unauthenticated request'
+    include_examples 'a request for a missing tweet'
+    include_examples 'a request from a different user'
   end
 end
