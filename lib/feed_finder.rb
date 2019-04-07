003756cf6e9b295c78386e28f56130799110a238
Matt Moriarity <matt@mattmoriarity.com>
2019-04-07 14:59:29 -0600

    Fix feed finder

--- lib/feed_finder.rb
+++ lib/feed_finder.rb
@@ -18,7 +18,7 @@ class FeedFinder
   end
 
   def attempt_url(url)
-    handle_response get_url(url)
+    handle_response url, get_url(url)
   end
 
   def get_url(url)
@@ -28,22 +28,22 @@ class FeedFinder
     }.get
   end
 
-  def handle_response(response)
+  def handle_response(url, response)
     case response.status
-    when 200 then handle_successful_response(response)
+    when 200 then handle_successful_response(url, response)
     when 404 then nil
     end
   end
 
-  def handle_successful_response(response)
+  def handle_successful_response(url, response)
     type = response.headers.fetch(:content_type, '')
 
     if %r{^text/html}.match?(type)
-      handle_html_response(response)
+      handle_html_response(url, response)
     else
       feed_type = FeedType.by_mime_type(type)
       if feed_type
-        feed_type.find(response)
+        feed_type.find(response) || url
       else
         Rails.logger.warn "Unexpected content type #{type}"
         nil
@@ -51,11 +51,15 @@ class FeedFinder
     end
   end
 
-  def handle_html_response(response)
+  def handle_html_response(url, response)
     html = Nokogiri::HTML(response.body)
     feed = FeedType.prioritized.lazy.map { |t|
       html.css(%(link[rel=alternate][type="#{t.mime_type}"])).first
     }.detect(&:itself)
-    attempt_url(feed['href']) if feed.present?
+
+    return nil if feed.blank?
+
+    href = URI.join(url, feed['href']).to_s
+    attempt_url(href)
   end
 end
